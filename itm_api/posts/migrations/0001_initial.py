5c29d32bea9a935da3730249fd7b9e7f88c3ba05
Rhys Young <rhysyoung16@gmail.com>
2021-04-26 22:29:52 -0400

    Adding linter

--- itm_api/posts/migrations/0001_initial.py
+++ itm_api/posts/migrations/0001_initial.py
@@ -7,18 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Book',
+            name="Book",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('author', models.CharField(max_length=50)),
-                ('year_published', models.CharField(max_length=10)),
-                ('review', models.PositiveIntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=100)),
+                ("author", models.CharField(max_length=50)),
+                ("year_published", models.CharField(max_length=10)),
+                ("review", models.PositiveIntegerField()),
             ],
         ),
     ]
